d2fd3efed30287caaaa18a2404843a4e6634c2a6
Мазерфакер <salihovegor1234gmail.com>
2026-04-21 20:39:10 +0300

    git init

---
+++ ts-longstory-landing/src/main.ts
@@ -0,0 +1,39 @@
+import './style.css';
+import './containers';
+
+const characterStats = [
+  {
+    nameofstat: "СИЛА",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+    competency: ["АТЛЕТИКА"]
+  },
+  {
+    nameofstat: "ЛОВКОСТЬ",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+    competency: ["АКРОБАТИКА", "ЛОВКОСТЬ РУК", "СКРЫТНОСТЬ"]
+  },
+  {
+    nameofstat: "ТЕЛОСЛОЖЕНИЕ",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+  },
+  {
+    nameofstat: "ИНТЕЛЛЕКТ",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+    competency: ["АНАЛИЗ", "ИСТОРИЯ", "МАГИЯ", "ПРИРОДА", "РЕЛИГИЯ"]
+  },
+  {
+    nameofstat: "МУДРОСТЬ",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+    competency: ["ВОСПРИЯТИЕ", "ВЫЖИВАНИЕ", "МЕДИЦИНА", "ПРИНИЦАТЕЛЬНОСТЬ", "УХОД ЗА ЖИВОТНЫМИ"]
+  },
+  {
+    nameofstat: "ХАРИЗМА",
+    checksave: ["ПРОВЕРКА", "СПАСБРОСОК"],
+    competency: ["ВЫСТУПЛЕНИЕ", "ЗАПУГИВАНИЕ", "ОБМАН", "УБЕЖДЕНИЕ"]
+  }
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+
+})
+
